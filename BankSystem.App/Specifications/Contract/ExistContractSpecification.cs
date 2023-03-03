e5d3a8ed6ffafcc3e314fa3766180ea76cce977e
Демиденко Иван Владимирович <ivdemidenk@mts.ru>
2023-03-03 10:14:14 +0200

    Косметическая микро редакция

--- BankSystem.App/Specifications/Contract/ExistContractSpecification.cs
+++ BankSystem.App/Specifications/Contract/ExistContractSpecification.cs
@@ -6,7 +6,8 @@ namespace BankSystem.App.Specifications.Contract;
 public class ExistContractSpecification : AndSpecification<Domain.Models.Contract>
 {
     public ExistContractSpecification(Guid contractId)
-        : base(new Specification<Domain.Models.Contract>(co => co.Id == contractId), new UndeleteContractSpecification())
+        : base(new Specification<Domain.Models.Contract>(co => co.Id == contractId),
+            new UndeleteContractSpecification())
     {
 
     }
